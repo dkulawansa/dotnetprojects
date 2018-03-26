faa19870e454783db065f4047c9baed96c959653
Digala M Kulawansa <dkulawansa@hotmail.com>
2018-03-25 21:12:27 -0400

    Initial commit

---
+++ SampleStack/SampleStack/StackProgram.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace SampleStack
+/*
+ * C# Program to Implement basic Stack operations
+ */
+{
+    public class StackProgram
+    {
+        static void Main(string[] args)
+        // Main program for user inputs
+        {
+            Stack myStack = new Stack();
+            while (true)
+            {
+                Console.Clear();
+                Console.WriteLine("\nStack MENU");
+                Console.WriteLine("1. Add an element");
+                Console.WriteLine("2. See the Top element.");
+                Console.WriteLine("3. Remove top element.");
+                Console.WriteLine("4. Display stack elements.");
+                Console.WriteLine("5. Total number of elements .");
+                Console.WriteLine("6. Is stack empty?");
+                Console.WriteLine("7. Exit");
+                Console.Write("Select your choice with value 1 to 7: ");
+
+                int choice = 0;
+                if (choice > 7 || choice < 0)
+                {
+                    choice = 0;
+                }
+                try
+                {
+                    choice = Convert.ToInt32(Console.ReadLine());
+                }
+                catch
+                {
+                    Console.Write("Enter a number from 1 to 7: ");
+                    choice = 0;
+                }
+                switch (choice)
+                {
+                    case 0:
+                        break;
+                    case 1:
+                        Console.WriteLine("Enter an Element : ");
+                        StackPush(Console.ReadLine(), myStack);
+                        break;
+
+                    case 2: Console.WriteLine("Top element is: {0}", StackPeek(myStack));
+                        break;
+
+                    case 3: Console.WriteLine("Element removed: {0}", StackPop(myStack));
+                        break;
+
+                    case 4: PrintValues(myStack, '\n');
+                        break;
+
+                    case 5: Console.WriteLine("Total Number of elements: {0}", StackCount(myStack));
+                        break;
+
+                    case 6: Console.WriteLine("\nIs stack empty?: {0}", IsStackEmpty(myStack));
+                        break;
+
+                    case 7: System.Environment.Exit(1);
+                        break;
+                }
+                Console.WriteLine("\nEnter any key ..... ");
+                Console.ReadKey();
+            }
+        }
+        public static void PrintValues(IEnumerable myCollection, char mySeparator)
+        // Display contents of Stack
+        {
+            foreach (Object obj in myCollection)
+                Console.Write("{0}{1}", mySeparator, obj);
+            Console.WriteLine();
+        }
+        public static void StackPush(Object element, Stack st)
+        // Pushes element to Stack
+        {
+            st.Push(element);
+        }
+        public static object StackPop(Stack st)
+        // Pops last element in the Stack
+        {
+            object obj = null;
+            if (st.Count != 0)
+                obj = st.Pop();
+            return obj;
+        }
+        public static object StackPeek(Stack st)
+        // Gives top element in the Stack
+        {
+            object obj = null;
+            if (st.Count != 0)
+                obj = st.Peek();
+            return obj;
+        }
+        public static Int32 StackCount(Stack st)
+        //Gives number of element(s) in the stack 
+        {
+            return st.Count;
+        }
+        public static bool IsStackEmpty(Stack st)
+        // Check stack is empty or not and returns true or false
+        {
+            bool flag = false;
+            if (st.Count == 0)
+                flag = true;
+            return flag;
+        }
+
+    }
+}
---
+++ SampleStack/StackTests/StackUnitTest.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections;
+using SampleStack;
+
+namespace StackTests
+{
+    [TestClass]
+    public class StackUnitTest
+    {   // Instantiate stack
+        Stack myStack = new Stack();
+
+        [TestMethod]
+        public void Stack_With_No_Push()
+        {
+            // Perform no operation on the stack and verify that the stack is empty
+            bool expected = true;
+            bool actual = StackProgram.IsStackEmpty(myStack);
+            // Assert that there are on elements in the stack
+            Assert.AreEqual(expected, actual, "Stack is empty ");
+        }
+
+        [TestMethod]
+        public void Stack_With_Valid_Pushes()
+        {
+            // Perform three push operations on the stack to arrange the test
+            StackProgram.StackPush(10, myStack);
+            StackProgram.StackPush(20, myStack);
+            StackProgram.StackPush(30, myStack);
+            int expected = 3;
+            int actual = StackProgram.StackCount(myStack);
+
+            // Assert that there are three elements in the stack
+            Assert.AreEqual(expected, actual, "Number of items pushed to stack");
+        }
+
+        [TestMethod]
+        public void Stack_Pops_Remove_Last_Push()
+        {
+            // Perform three push operations on the stack to arrange the test
+            StackProgram.StackPush(10, myStack);
+            StackProgram.StackPush(20, myStack);
+            StackProgram.StackPush(30, myStack);
+            object expected = 30;
+            int expected_count = 2;
+            object actual = StackProgram.StackPop(myStack);
+            int actual_count = StackProgram.StackCount(myStack);
+
+            // Verify that there are two element in the stack after Pop operation
+            Assert.AreEqual(expected, actual, "Number of items pushed to stack");
+            Assert.AreEqual(expected_count, actual_count, "Number of items after a Pop");
+        }
+
+        [TestMethod]
+        public void Stack_Peek_Give_Top_Item_No_Change_to_Stack()
+        {
+            // Perform three push operations on the stack to arrange the test
+            StackProgram.StackPush(10, myStack);
+            StackProgram.StackPush(20, myStack);
+            StackProgram.StackPush(30, myStack);
+            object expected = 30;
+            object actual = StackProgram.StackPeek(myStack);
+            int expected_count = 3;
+            int actual_count = StackProgram.StackCount(myStack);
+
+            // Verify that there are all three elements in the stack after Peek operation
+            Assert.AreEqual(expected, actual, "Number of items pushed to stack is 3");
+            Assert.AreEqual(expected_count, actual_count, "Number of items after a Pop");
+        }
+    }
+}
